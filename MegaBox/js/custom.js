177f9ccba1ddf749078495fb96b40d1b1b427097
sryung1225 <sryung1225@gmail.com>
2020-11-25 23:51:54 +0900

    05 swiper.js

---
+++ MegaBox/js/custom.js
@@ -0,0 +1,13 @@
+/* swiper.js */
+var swiper = new Swiper('.swiper-container',{
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+        delay: 5000,
+    },
+});
