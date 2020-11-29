5ba01250f3ab1a8c87f636c014336d5711519448
sryung1225 <sryung1225@gmail.com>
2020-11-30 01:12:19 +0900

    08 영화 차트 탭 메뉴

--- MegaBox/js/custom.js
+++ MegaBox/js/custom.js
@@ -1,4 +1,4 @@
-/* swiper.js */
+/* 배너 이미지 슬라이드 - swiper.js */
 var swiper = new Swiper('.swiper-container',{
     pagination: {
         el: '.swiper-pagination',
@@ -10,4 +10,21 @@ var swiper = new Swiper('.swiper-container',{
     autoplay: {
         delay: 5000,
     },
+});
+
+/* 영화차트 탭 메뉴 */
+var movBtn = $(".movie_title > ul > li");
+var movCont = $(".movie_chart > div");
+
+movCont.hide().eq(0).show();
+
+movBtn.click(function(e){
+    e.preventDefault();
+    var target = $(this);
+    var index = target.index();
+    /* alert(index); */
+    movBtn.removeClass("active");
+    target.addClass("active");
+    movCont.css("display","none");
+    movCont.eq(index).css("display","block");
 });
