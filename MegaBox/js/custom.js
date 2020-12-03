b4550ef845de1b011da6314c47f34a6c21b92e1f
sryung1225 <sryung1225@gmail.com>
2020-12-04 01:26:19 +0900

    21 Youtube API

--- MegaBox/js/custom.js
+++ MegaBox/js/custom.js
@@ -1,76 +1,79 @@
-/* 배너 이미지 슬라이드 - swiper.js */
-var swiper = new Swiper('.swiper-container',{
-    pagination: {
-        el: '.swiper-pagination',
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    autoplay: {
-        delay: 5000,
-    },
-});
-
-/* 영화 차트 이미지 슬라이드 - swiper.js */
-var swiper2 = new Swiper('.swiper-container2',{
-    slidesPerView: 4,
-    spaceBetween: 24,
-    mousewheel: {
-        invert: true,
-    },
-    keyboard: {
-        enabled: true,
-        onlyInViewport: false,
-    },
-    autoplay: {
-        delay: 6000,
-    },
-    breakpoints: {
-        600: {
-            slidesPerView: 1.4,
-            spaceBetween: 24
+(function($){
+    
+    /* 배너 이미지 슬라이드 - swiper.js */
+    var swiper = new Swiper('.swiper-container',{
+        pagination: {
+            el: '.swiper-pagination',
         },
-        768: {
-            slidesPerView: 2,
-            spaceBetween: 24
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
         },
-        960: {
-            slidesPerView: 3,
-            spaceBetween: 24
-        }
-    },
-});
+        autoplay: {
+            delay: 5000,
+        },
+    });
+
+    /* 영화 차트 이미지 슬라이드 - swiper.js */
+    var swiper2 = new Swiper('.swiper-container2',{
+        slidesPerView: 4,
+        spaceBetween: 24,
+        mousewheel: {
+            invert: true,
+        },
+        keyboard: {
+            enabled: true,
+            onlyInViewport: false,
+        },
+        autoplay: {
+            delay: 6000,
+        },
+        breakpoints: {
+            600: {
+                slidesPerView: 1.4,
+                spaceBetween: 24
+            },
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 24
+            },
+            960: {
+                slidesPerView: 3,
+                spaceBetween: 24
+            }
+        },
+    });
 
 
-/* 영화차트 탭 메뉴 */
-var movBtn = $(".movie_title > ul > li");
-var movCont = $(".movie_chart > div");
+    /* 영화차트 탭 메뉴 */
+    var movBtn = $(".movie_title > ul > li");
+    var movCont = $(".movie_chart > div");
 
-movCont.hide().eq(0).show();
+    movCont.hide().eq(0).show();
 
-movBtn.click(function(e){
-    e.preventDefault();
-    var target = $(this);
-    var index = target.index();
-    /* alert(index); */
-    movBtn.removeClass("active");
-    target.addClass("active");
-    movCont.css("display","none");
-    movCont.eq(index).css("display","block");
-});
+    movBtn.click(function(e){
+        e.preventDefault();
+        var target = $(this);
+        var index = target.index();
+        /* alert(index); */
+        movBtn.removeClass("active");
+        target.addClass("active");
+        movCont.css("display","none");
+        movCont.eq(index).css("display","block");
+    });
 
 
-/* 공지사항 탭 메뉴 */
-var tabMenu = $(".notice");
+    /* 공지사항 탭 메뉴 */
+    var tabMenu = $(".notice");
 
-tabMenu.find("ul > li > ul").hide();
-tabMenu.find("ul > li.active > ul").show();
+    tabMenu.find("ul > li > ul").hide();
+    tabMenu.find("ul > li.active > ul").show();
 
-function tabList(e){
-    e.preventDefault();
-    var target = $(this);
-    target.next().show().parent("li").addClass("active").siblings("li").removeClass("active").find("ul").hide();
-}
+    function tabList(e){
+        e.preventDefault();
+        var target = $(this);
+        target.next().show().parent("li").addClass("active").siblings("li").removeClass("active").find("ul").hide();
+    }
 
-tabMenu.find("ul > li > a").click(tabList).focus(tabList);
+    tabMenu.find("ul > li > a").click(tabList).focus(tabList);
+})(jQuery); 
